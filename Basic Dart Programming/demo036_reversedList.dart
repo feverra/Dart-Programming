9ed0a6a6cdd2475c87eda54e854baa9c31e879c3
Tizcom <45397970+Tizcom@users.noreply.github.com>
2021-04-07 19:30:35 +0700

    Delete Basic Dart Programming directory

--- Basic Dart Programming/demo036_reversedList.dart
+++
@@ -1,6 +0,0 @@
-void main() {
-  var vals = <int>[10, 20, 30, 40, 50];
-
-  var reversed = List.of(vals.reversed);
-  print(reversed);
-}
--- Basic Dart Programming/demo044_singleMemberList.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  var mylist = [];
-
-  mylist.add(10);
-  print("List has only one Element is : ${mylist.single}");
-
-  mylist.add(20);
-  print("List has only one Element is : ${mylist.single}"); // Error
-}
